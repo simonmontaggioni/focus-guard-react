0bbdb7bd0899da95bc6c72a21570c03a51bd5734
simon.montaggioni <simon.montaggioni@gmail.com>
2020-09-21 11:01:00 -0400

    feat: Create HomePage component.
     - Install node-sass.
     - Set the forlder structure.
     - Create the HomePage component and its scss file.
     - Remove unnecessary files.
     - Change the logo and favicon.

---
+++ src/pomodoro/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/HomePage.scss';
+
+const HomePage = () => {
+  return (
+    <div className='container'>
+      <h1>Focus Guard: a pomodoro timer.</h1>
+    </div>
+  );
+};
+
+export default HomePage;
