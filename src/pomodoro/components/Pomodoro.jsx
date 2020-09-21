6f59ef587e05053ef5c1a53eb716eb6275818f3f
simon.montaggioni <simon.montaggioni@gmail.com>
2020-09-21 14:35:46 -0400

    feat: Include the Button component into the Pomodoro.
      - Improve the layout.

--- src/pomodoro/components/Pomodoro.jsx
+++ src/pomodoro/components/Pomodoro.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import Display from './Display';
 import '../css/Pomodoro.scss';
 import TimeControl from './TimeControl';
+import Button from './Button';
 
 const Pomodoro = () => {
   return (
     <div className='pomodoro'>
-      <div>
+      <div className='first-row'>
         <TimeControl controlType={'Break'} />
       </div>
-      <Display />
-      <div>
+      <div className='second-row'>
+        <Button text={'Reset'} />
+        <Display />
+        <Button text={'Play/Pause'} />
+      </div>
+      <div className='third-row'>
         <TimeControl controlType={'Focus'} />
       </div>
     </div>
