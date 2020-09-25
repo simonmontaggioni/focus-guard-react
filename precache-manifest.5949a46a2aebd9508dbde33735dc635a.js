9af1ece703ee2276b1a84ff2fcd7dc288316df56
simon.montaggioni <simon.montaggioni@gmail.com>
2020-09-25 16:08:27 -0400

    Updates

---
+++ precache-manifest.5949a46a2aebd9508dbde33735dc635a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31026eed01efd1e9137e1fca0cfe74e",
+    "url": "/focus-guard-react/index.html"
+  },
+  {
+    "revision": "64d96ddbe188f1ee83df",
+    "url": "/focus-guard-react/static/css/main.0ca42dd1.chunk.css"
+  },
+  {
+    "revision": "d03820702257a582135e",
+    "url": "/focus-guard-react/static/js/2.cd6aad0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/focus-guard-react/static/js/2.cd6aad0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d96ddbe188f1ee83df",
+    "url": "/focus-guard-react/static/js/main.59d509be.chunk.js"
+  },
+  {
+    "revision": "4eff5242f48104a7c345",
+    "url": "/focus-guard-react/static/js/runtime-main.4fe9d470.js"
+  },
+  {
+    "revision": "07943afe4809207565b3362d474c4866",
+    "url": "/focus-guard-react/static/media/7segments.07943afe.ttf"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/focus-guard-react/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "f568f29616d99885b4a8d0e84a08eb29",
+    "url": "/focus-guard-react/static/media/CaveatBrush-Regular.f568f296.ttf"
+  },
+  {
+    "revision": "1408458d90c71d86a6b332852a1fa8f1",
+    "url": "/focus-guard-react/static/media/Japanese-Temple-Bell-Small.1408458d.wav"
+  },
+  {
+    "revision": "bea35746fb7cdd67b15ecf0622d8cc85",
+    "url": "/focus-guard-react/static/media/Kalam-Regular.bea35746.ttf"
+  },
+  {
+    "revision": "8bef9fabc8eec1371a7d30b7fe07a9d5",
+    "url": "/focus-guard-react/static/media/background.8bef9fab.jpg"
+  }
+]);
